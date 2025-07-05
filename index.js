5a0e8de2b9dc42f672ad976b706917056205c419
G57890 <158786556+G57890@users.noreply.github.com>
2025-07-05 22:27:39 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const dec = document.getElementById("dec");
+const res = document.getElementById("res");
+const inc = document.getElementById("inc");
+const counter = document.getElementById("h1-counter");
+let count = 0;
+
+dec.onclick = function() {
+    count--;
+    counter.textContent = count
+}
+
+res.onclick = function() {
+    count = 0;
+    counter.textContent = count
+}
+
+inc.onclick = function() {
+    count++;
+    counter.textContent = count
+}
